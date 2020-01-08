4fb5a096dbe70c0bc34f7a05e4df6b27b7ee520a
Pablo Ariel Bilevich Alaniz <pablobvch@hotmail.com>
2020-01-08 15:18:16 -0300

    feat: first commit

---
+++ src/server/index.mjs
@@ -0,0 +1,30 @@
+//const express = require("express");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ status: "success", message: "Welcome To Testing API" });
+});
+
+app.post("/add", (req, res) => {
+  const { num1, num2 } = req.body;
+
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+
+  res.json({
+    status: "success",
+    result: "Welcome To Testing API",
+    result: add(num1, num2)
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+
+export default app;
